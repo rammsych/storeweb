f252917d018307451836766c4f49c9a1f09c74e8
Christian Rammsy <52750142+rammsych@users.noreply.github.com>
2026-05-22 11:13:37 -0400

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export async function POST(request) {
   try {
